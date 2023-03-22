d23bc34590f469e6b695f8bd3fc48838463aa622
c+1 <89845610+s-over-4@users.noreply.github.com>
2023-03-22 01:16:46 -0400

    got basic functionality down

--- main.py
+++ main.py
@@ -1,90 +1,152 @@
+__version__ = "2.0"
+
 import basebot
 import sqlite3
+from time import time
+from string import Template
 
 
 
-NICK = 'qb'             # nick that the bot will assume when connecting to a room
+# ===== quick config ===== #
+#                          #
 
-DB_NAME = 'quotes.db'   # name that the quote database will be read from
+NICK = 'qb'                     # nick that the bot will send from and respond to (WILL APPEAR IN DATABASE)
+MAINTAINER = 'c+1'              # the name of the person maintaining this bot (WILL APPEAR IN DATABASE)
 
-LONG_HELP = (           # long help message
+LONG_HELP = (                   # long help message
     'a quote bot by @c+1\n'
     'commands:\n'
-    '   !quote - quote the parent message\n'
-    '   !quote "message" @user - attribute a quote to a specified user\n'
-    '   !list @qb - list quotes\n'
+    '   !quote -parent:     quote the parent message\n'
+    '   !quote -list:       list last 15 quotes\n'
+    '   !quote:             alias for !quote -parent\n'
 )
+SHORT_HELP = 'a quote bot'      # short help message
 
-SHORT_HELP = (          # short help message
-    'a quote bot'
-)
+DB_NAME = 'quotes.db'           # name of the file that the quote database will be written to/read from
+
+#                              #
+# ===== end quick config ===== #
+
+
 
 
 
 # ===== setup database ===== #
+#                            #
 
 queries = {
     'init_db': (
-        'CREATE TABLE IF NOT EXISTS quotes('
-        '   q_id INTEGER PRIMARY KEY,'          # the id of the quote in the database
-        '   q_time INTEGER NOT NULL,'           # the time at which the quote was recorded; epoch timestamp
-        '   q_content TEXT NOT NULL,'           # the quote content
-        '   q_author TEXT NOT NULL,'            # the quote author
-        '   euph_room TEXT,'                    # the room on euphoria.io where the quote was recorded
-        '   euph_id TEXT,'                      # the id of the message on euphoria.io in the specified room
-        '   platform TEXT,'                     # the platform the quote was transcribed from
-        '   edited NUMERIC'                     # whether any part of the quote has been edited after initial transcription
-        ')'
+        'CREATE TABLE IF NOT EXISTS quotes ('
+
+        # === quote parameters === #
+        '   q_id INTEGER PRIMARY KEY AUTOINCREMENT,'    # the id of the quote in the database
+        '   q_time INTEGER NOT NULL,'                   # the time at which the quoted message was posted; epoch timestamp
+        '   q_content TEXT NOT NULL,'                   # the quote content
+        '   q_author TEXT NOT NULL,'                    # the quote author
+
+        # === euphoria-specific === #
+        '   euph_room TEXT,'                            # the room on euphoria.io where the quote was recorded
+        '   euph_id TEXT,'                              # the id of the message on euphoria.io in the specified room
+        '   euph_bot TEXT,'                             # the name of the bot that recorded the quote on euphoria.io
+        '   euph_maintainer TEXT,'                      # the name of the person maintaining the bot
+
+        # === misc === #
+        '   platform TEXT,'                             # the platform the quote was transcribed from
+        '   edited NUMERIC'                             # whether any part of the quote has been edited after initial transcription
+        ');'
+    ),
+
+    'add_quote': (
+        'INSERT INTO quotes (q_time, q_content, q_author, euph_room, euph_id, euph_bot, euph_maintainer, platform, edited)'
+        'VALUES (            ?,      ?,         ?,        ?,         ?,       ?,        ?,              \'euphoria.io\', 0 );'
+    ),
+
+    'list_15': (
+        'SELECT * FROM quotes ORDER BY q_time DESC LIMIT 15'
     )
 }
-
 
 
 con = sqlite3.connect(DB_NAME, check_same_thread=False)
 cur = con.cursor()
 
-cur.execute(queries['init_db'], )
+cur.execute(queries['init_db'])
+
+#                                #
+# ===== end setup database ===== #
+
+
+
+
+
+# ===== helper functions ===== #
+#                              #
+
+# get_parent: Dict meta -> Object
+# get the parent message details
+def get_parent(meta):
+    parent_id = meta['msg'].parent
+
+    # === api call for parent message === #
+    packet = { 'type': 'get-message', 'data': {'id': parent_id} }
+    basebot.BaseBot.send_raw( self=meta['self'], obj=packet, retry=True )
+    parent_data = basebot.BaseBot.recv_raw( self=meta['self'], retry=True )
+
+    return parent_data
+
+#                                  #
+# ===== end helper functions ===== #
+
+
 
 
 
 # ===== command functions ===== #
+#                               #
 
 # !quote - quote parent message
 # regex: ^!quote$
 def quote_parent(match, meta):
-    # === get parent message id === #
-    parent = meta['msg'].parent
-
-    # ==+ api call for parent message === #
-    packet = {'type': 'get-message', 'data': {'id': parent}}
-    basebot.BaseBot.send_raw( self=meta['self'], obj=packet, retry=True )
-    parent_data = basebot.BaseBot.recv_raw( self=meta['self'], retry=True )
+    parent_data = get_parent(meta)
     
-    # === check if there is a parent message === #
-    if 'error' in parent_data: meta['reply']('error: ' + parent_data['error']); return
-    content = parent_data['data']['content']
-    meta['reply'](content)
+    # === check if there's a parent message at all === #
+    if 'error' in parent_data: meta['reply']('API error: ' + parent_data['error']); return
 
-    # === create query === #
-    # q_id, date, room, msg_content, msg_id, usr_nick, usr_id, edited
-    q_id = (cur.execute('SELECT MAX(q_id) FROM quotes').fetchall())[0]
-    meta['reply']('q_id:' + str(q_id))
+    # === prepare quote details === #
+    q_time = parent_data['data']['time']
+    q_content = parent_data['data']['content']
+    q_author = parent_data['data']['sender']['name']
+
+    euph_room = meta['msg_meta']['self'].roomname
+    euph_id = parent_data['data']['id']
+
+    cur.execute(
+        queries['add_quote'], (
+            # for the most part, these mach their field names
+            q_time,
+            q_content,
+            q_author,
+            euph_room,
+            euph_id,
+            NICK,       # euph_bot
+            MAINTAINER  # maintainer
+        )
+    )
+
+    con.commit()
+
+    meta['reply']("added successfully")
     
 
-
-# !list - list quotes
+# !list - list last 15 quotes
 # regex: ^!list\s+@(\S+)$
 def list(match, meta):
-    res = cur.execute('SELECT q_content FROM quotes')
-    meta['reply'](str(res.fetchall()))
+    response = ''
+    for row in cur.execute(queries['list_15']):
+        response += f"\"{row[2]}\" - {row[3]}\n"
+    
+    meta['reply'](response)
 
-
-# !test_the
-# regex: ^!test_the$
-def test_the(match, meta):
-    meta['reply']('testing...')
-    res = cur.execute('SELECT name FROM sqlite_master')
-    meta['reply'](str(res.fetchone()))
 
 
 # !kill command
@@ -104,15 +166,20 @@ def maybe_die(match, meta):
 # regexes to which the bot will respond
 regexes = {
     r'^!kill\s+@(\S+)\s*$': maybe_die,
-    r'^!test_the$': test_the,
-    r'^!list\s+@(\S+)$': list,
-    r'^!quote$': quote_parent
+    r'^!quote\s+-list$': list,
+    r'^!quote$': quote_parent,
+    r'^!quote\s+-parent$': quote_parent
 }
+
+#                                   #
+# ===== end command functions ===== #
+
+
 
 
 
 def main():
-    # === run bot === #
+    # === run the bot! === #
     basebot.run_minibot(
         botname=NICK,
         nickname=NICK,
